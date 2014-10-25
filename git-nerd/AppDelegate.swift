85057513263e12be8c5fc0f9335cd72a6d1f25b2
Berkus Decker <berkus@madfire.net>
2014-10-25 14:32:51 +0300

    Add Xcode6 project.

---
+++ git-nerd/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  git-nerd
+//
+//  Created by Berkus on 25/10/14.
+//  Copyright (c) 2014 Atta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ git-nerd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  git-nerd
+//
+//  Created by Berkus on 25/10/14.
+//  Copyright (c) 2014 Atta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
