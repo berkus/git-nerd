2ca585b1db9462f2e54e91dc0a4815eb69deabe0
Berkus Decker <berkus@madfire.net>
2014-10-26 12:11:08 +0200

    Add bridging header to import ObjectiveGit.

---
+++ git-nerd/git_nerd-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  git-nerd-Bridging-Header.h
+//  git-nerd
+//
+//  Created by Berkus on 26/10/14.
+//  Copyright (c) 2014 Atta. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import <ObjectiveGit/ObjectiveGit.h>
